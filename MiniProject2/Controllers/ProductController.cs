fcceab6a26c0bd9685bb4f267d647847834e1d98
Pratiksha126 <pratikshabhosale7066@gmail.com>
2024-06-12 22:45:44 +0530

    poductchanges

--- MiniProject2/Controllers/ProductController.cs
+++ MiniProject2/Controllers/ProductController.cs
@@ -7,11 +7,16 @@ namespace MiniProject2.Controllers
 {
     public class ProductController : Controller
     {
-        private readonly IProductService service;  
+        private readonly IProductService service;
+        private readonly ICategoryService service1;
+        private Microsoft.AspNetCore.Hosting.IHostingEnvironment env;
 
-        public ProductController(IProductService service)
+        public ProductController(IProductService service, ICategoryService service1, Microsoft.AspNetCore.Hosting.IHostingEnvironment e)
         {
             this.service = service;
+            this.service1 = service1;
+            this.env = e;
+
         }
         // GET: ProductController
         public ActionResult Index()
@@ -39,12 +44,16 @@ namespace MiniProject2.Controllers
         // POST: ProductController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, IFormFile file)
         {
-            using (var fs = new FileStream(_iHostEnv.WebRootPath + "\\images\\" + file.FileName, FileMode.Create, FileAccess.Write))
+            using (var fs = new FileStream(env.WebRootPath + "\\images\\" + file.FileName, FileMode.Create, FileAccess.Write))
             {
                 file.CopyTo(fs);
             }
+            product.ImageURL = "~/images/" + file.FileName;
+        
+
+
             try
             {
                 int result = service.AddProduct(product);
@@ -70,6 +79,7 @@ namespace MiniProject2.Controllers
         public ActionResult Edit(int id)
         {
             var product = service.GetProductById(id);
+            HttpContext.Session.SetString("oldImageURL", product.ImageURL);
 
             return View(product);
             
@@ -78,8 +88,26 @@ namespace MiniProject2.Controllers
         // POST: ProductController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(Product product)
+        public ActionResult Edit(Product product, IFormFile file)
         {
+            string oldimageurl = HttpContext.Session.GetString("oldImageUrl");
+            if (file != null)
+            {
+                using (var fs = new FileStream(env.WebRootPath + "\\images\\" + file.FileName, FileMode.Create, FileAccess.Write))
+                {
+                    file.CopyTo(fs);
+                }
+                product.ImageURL = "~/images/" + file.FileName;
+
+                string[] str = oldimageurl.Split("/");
+                string str1 = (str[str.Length - 1]);
+                string path = env.WebRootPath + "\\images\\" + str1;
+                System.IO.File.Delete(path);
+            }
+            else
+            {
+                product.ImageURL = oldimageurl;
+            }
             try
             {
                 int result = service.UpdateProduct(product);
